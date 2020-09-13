d497842702887db759bf1fb3020d606bc35e82fe
Ray Douglass <ray@raydouglass.com>
2020-09-13 14:34:20 -0400

    Step #3

--- core/models.py
+++ core/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
 
 class Author(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+    birth_date = models.DateField(default=None, null=True, blank=True)
+
 
 class Book(models.Model):
     title = models.CharField(max_length=128)
-    authors = models.ManyToManyField(Author, related_name='books') #related_name is easier than author_set
+    year = models.PositiveIntegerField()
+    authors = models.ManyToManyField(Author, related_name='books')  # related_name is easier than author_set
+
+    class Meta:
+        unique_together = ['title', 'year']
