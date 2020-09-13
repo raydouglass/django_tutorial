74025a0f20a433e762aad5cef348a8efff450708
Ray Douglass <ray@raydouglass.com>
2020-09-13 14:28:23 -0400

    Step #2

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+class Book(models.Model):
+    title = models.CharField(max_length=128)
+    authors = models.ManyToManyField(Author, related_name='books') #related_name is easier than author_set
